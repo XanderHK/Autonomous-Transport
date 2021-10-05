4668ff16aa2347b319bcf7f483dffb97457da875
XanderHK <xander.korts@gmail.com>
2021-10-05 20:16:26 +0200

    [FTR] Raycaster to see if clicks intersect with objects

--- src/scenes/world.ts
+++ src/scenes/world.ts
@@ -1,9 +1,8 @@
-import { AnimationMixer, Camera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as THREE from 'three'
 import CharacterControls from '../controls/characterControls';
+import { Raycaster, Vector2 } from 'three';
 
 class World {
 
@@ -16,6 +15,8 @@ class World {
     private _controls?: OrbitControls
     private _keysPressed: { [key: string]: boolean }
     private _characterControls?: CharacterControls;
+    private _raycaster: Raycaster
+    private _mouse: Vector2
 
     constructor() {
         this._renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -23,6 +24,8 @@ class World {
         this._scene = new THREE.Scene()
         this._clock = new THREE.Clock()
         this._keysPressed = {}
+        this._raycaster = new Raycaster()
+        this._mouse = new Vector2()
 
         this._Init()
     }
@@ -35,9 +38,26 @@ class World {
         this._renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this._renderer.domElement);
 
+        this._AddListeners()
+        this._Camera()
+        this._Lighting()
+        this._Controls()
+        this._Skybox()
+        this._Plane()
+        this._LoadAnimatedModel()
+
+        this._RAF()
+    }
+
+    private _AddListeners() {
         window.addEventListener('resize', () => {
             this._OnWindowResize();
         }, false);
+
+        window.addEventListener('mousedown', (e) => {
+            this._mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+            this._mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+        }, false)
 
         window.addEventListener('keydown', (e: KeyboardEvent) => {
             if (e.shiftKey && this._characterControls) {
@@ -50,15 +70,6 @@ class World {
         window.addEventListener('keyup', (e: KeyboardEvent) => {
             this._keysPressed[e.key.toLowerCase()] = false
         }, false)
-
-        this._Camera()
-        this._Lighting()
-        this._Controls()
-        this._Skybox()
-        this._Plane()
-        this._LoadAnimatedModel()
-
-        this._RAF()
     }
 
     private _Camera() {
@@ -122,6 +133,7 @@ class World {
         plane.receiveShadow = true;
         plane.rotation.x = -Math.PI / 2;
         this._scene.add(plane);
+
     }
 
     private _LoadAnimatedModel() {
@@ -176,13 +188,17 @@ class World {
 
     private _RAF() {
         requestAnimationFrame(() => {
+            this._raycaster.setFromCamera(this._mouse, this._camera);
+            const intersects = this._raycaster.intersectObjects(this._scene.children);
+            for (let i = 0; i < intersects.length; i++) {
+                (<THREE.MeshStandardMaterial>(<THREE.Mesh>intersects[i].object).material).color.set(0xff0000)
+            }
             if (this._mixer !== undefined) this._mixer.update(this._clock.getDelta())
             if (this._characterControls) this._characterControls._Update(this._clock.getDelta(), this._keysPressed)
             this._renderer.render(this._scene, this._camera);
             this._RAF();
         });
     }
-
 }
 
 export default World
